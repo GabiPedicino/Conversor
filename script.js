86ddad08e192062273b9f354817817e229ed832b
Gabriela <gabrielapedicino@gmail.com>
2021-09-27 17:23:09 -0300

    Se agregó un nuevo conversor y se realizaron las funcionalidades correspondientes en Javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function conversor(){
     let pesosIngresados = document.getElementById("pesos").value;
-    let dolarHoy = 97.25;
+    let dolarHoy = 97.75;
     let impuestoPais;
     let impuestoGanancias;
     let valorTotal;
@@ -11,20 +11,9 @@ function conversor(){
     impuestoGanancias = pesosIngresados * 0.35;
     valorTotal = parseFloat (pesosIngresados) + parseFloat (impuestoPais) + parseFloat (impuestoGanancias); //valor que abona con la suma de los impuestos.
 
-    
-    
-
-    if(resultado>6000)
-    {
-        
-        alert('No se puede convertir un monto superior a 6000 dólares');
-
-    }
-    else
-    {
-       alert('$' + pesosIngresados + ' pesos argentinos son U$S' + resultado.toFixed(3) + '\n' + 
+       alert('$' + pesosIngresados + ' pesos argentinos son U$S' + resultado.toFixed(2) + '\n' + 
        'El impuesto país es de: $' + impuestoPais.toFixed(2) + '\n' + 'El recargo impositivo es de: $' + impuestoGanancias.toFixed(2) + '\n' + 'El total a pagar por U$S' + resultado.toFixed(2) + ' es de $' + valorTotal.toFixed(2));
-    }
+    
 
 }
 
@@ -37,4 +26,10 @@ function imposeMinMax(el){
         el.value = el.max;
       }
     }
-  }
+}
+
+function limpiarMonto() {
+  document.getElementById("pesos").value = "";
+}
+
+
---
+++ scriptdos.js
@@ -0,0 +1,38 @@
+function dolarAPesos(){
+    let dolarIngresado = parseFloat(document.getElementById("dolar").value);
+    let dolarHoyDos = 97.75;
+    let resultadoDos;
+    let impuestoPaisDos;
+    let impuestoGananciasDos;
+    let valorTotalPesos;
+    
+  
+    resultadoDos = parseFloat(dolarIngresado * dolarHoyDos);
+    impuestoPaisDos = dolarIngresado * 0.3;
+    impuestoGananciasDos = dolarIngresado * 0.35;
+    valorTotalPesos = parseFloat(resultadoDos) + parseFloat(impuestoPaisDos) + parseFloat(impuestoGananciasDos);
+  
+    console.log("funciona")
+    
+    alert('U$S' + dolarIngresado.toFixed(2) + ' dolares ingresados son $' + resultadoDos.toFixed(2) + '\n' + 
+   'El impuesto país es de: U$S' + impuestoPaisDos.toFixed(2) + '\n' + 'El recargo impositivo es de: U$S' + impuestoGananciasDos.toFixed(2) + '\n' + 'El total a cobrar por U$S' + dolarIngresado.toFixed(2) + ' es de $' + valorTotalPesos.toFixed(2));
+  }
+
+function imposeMinMax(el){
+    if(el.value != "")
+    {
+      if(parseInt(el.value) < parseInt(el.min))
+      {
+        el.value = el.min;
+      }
+      if(parseInt(el.value) > parseInt(el.max))
+      {
+        el.value = el.max;
+      }
+    }
+    console.log("entra");
+}
+
+function limpiar() {
+    document.getElementById("dolar").value = "";
+}
