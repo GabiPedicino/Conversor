40e004d1ad39a28f447783a77c4ebba7a76793de
Gabriela <gabrielapedicino@gmail.com>
2024-02-26 21:20:36 -0300

    se agrego API, se modifico archivo js y estilos

--- script.js
+++ script.js
@@ -1,33 +1,73 @@
-const conversor = () => {
-  let pesosIngresados = document.getElementById("pesos").value;
-  let dolarHoy = 1065.00;
-  let impuestoPais;
-  let impuestoGanancias;
-  let valorTotal;
- 
-
-  resultado = pesosIngresados / dolarHoy;
-  impuestoPais = pesosIngresados * 0.3;
-  impuestoGanancias = pesosIngresados * 0.35;
-  valorTotal = parseFloat (pesosIngresados) + parseFloat (impuestoPais) + parseFloat (impuestoGanancias); //valor que abona con la suma de los impuestos.
-
-  alert('$ ' + pesosIngresados + ' pesos argentinos son U$S ' + resultado.toFixed(2) + '\n' + 
-  'El impuesto país es de: $ ' + impuestoPais.toFixed(2) + '\n' + 'El recargo impositivo es de: $ ' + impuestoGanancias.toFixed(2) + '\n' + 'El total a pagar por U$S ' + resultado.toFixed(2) + ' es de $ ' + valorTotal.toFixed(2));
-  
-
-}
-
-const imposeMinMax = (el) =>{
-  if(el.value != ""){
-    if(parseInt(el.value) < parseInt(el.min)){
-      el.value = el.min;
-    }
-    if(parseInt(el.value) > parseInt(el.max)){
-      el.value = el.max;
-    }
+const pesosADolar = () => {
+  let pesosIngresados = parseFloat(document.getElementById("pesos").value);
+  console.log("Pesos ingresados:", pesosIngresados);
+  if (isNaN(pesosIngresados)) {
+    alert("El valor ingresado no es un número válido.");
+    return;
   }
+  fetch("https://dolarapi.com/v1/dolares/blue")
+    .then(response => {
+      console.log("Respuesta de la API:", response);
+      if (!response.ok) {
+        throw new Error('La respuesta de la API no fue exitosa.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log("Datos de la API:", data);
+      let dolarHoy = parseFloat(data.compra);
+      console.log('Compra: ', data.compra);
+      let dolarHoyVenta = parseFloat(data.venta);
+      console.log('Venta: ', data.venta);
+      if (isNaN(dolarHoy)) {
+        throw new Error('El valor del dólar obtenido de la API no es un número válido.');
+      }
+      let cotizacion = (dolarHoy + dolarHoyVenta) / 2; 
+      let valorTotal = pesosIngresados / cotizacion;     
+      alert("Cotizacion dólar: U$S " + cotizacion + '\n' + "Pesos ingresados: $ " + pesosIngresados + '\n' + "Recibirá: U$S " + valorTotal.toFixed(2));
+    })
+    .catch(error => console.error('Error al obtener el valor del dólar', error));
 }
+
+
+
+const dolarAPesos = () => {
+  let dolaresIngresados = parseFloat(document.getElementById("dolar").value);
+  console.log("Dólares ingresados:", dolaresIngresados);
+  if (isNaN(dolaresIngresados)) {
+    alert("El valor ingresado no es un número válido.");
+    return;
+  }
+
+  fetch("https://dolarapi.com/v1/dolares/blue")
+    .then(response => {
+      console.log("Respuesta de la API:", response);
+      if (!response.ok) {
+        throw new Error('La respuesta de la API no fue exitosa.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log("Datos de la API:", data);
+      let dolarHoy = parseFloat(data.compra);
+      console.log('Compra: ', data.compra);
+      let dolarHoyVenta = parseFloat(data.venta);
+      console.log('Venta: ', data.venta);
+      if (isNaN(dolarHoy)) {
+        throw new Error('El valor del dólar obtenido de la API no es un número válido.');
+      }
+      let cotDolar = (dolarHoy + dolarHoyVenta) / 2;
+      let resultado = dolaresIngresados * cotDolar;
+      alert("Cotizacion dólar: U$S " + cotDolar + '\n' + "Dólares ingresados: U$S " + dolaresIngresados + '\n' + "Recibirá: $ " + resultado.toFixed(2));
+    })
+    .catch(error => console.error('Error al obtener el valor del dólar:', error));
+}
+
 
 const limpiarMonto = () => {
-document.getElementById("pesos").value = "";
+  document.getElementById("pesos").value = "";
+}
+
+const limpiar = () =>{
+  document.getElementById("dolar"), value = "";
 }
